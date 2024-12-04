b1f9164ec2a3983579912d5032b67009c504d3ca
Ornnishsha <starlightpink2021@gmail.com>
2024-12-04 23:55:17 +0700

    Update nlp_app.py

--- nlp_app.py
+++ nlp_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.title('writing tutor')
-st.title('hello')
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+    
